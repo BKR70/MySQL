7068a5ec5e39e029b07c44ca6ce363022e8fe8d8
Bilas Kumar <153761069+BKR70@users.noreply.github.com>
2025-05-20 12:54:34 +0600

    Update inner_join

--- JOIN/inner_join.sql
+++ JOIN/inner_join.sql
@@ -20,3 +20,20 @@ INNER JOIN EnrolledIn E ON S.id = E.sid
 INNER JOIN CourseDetails C ON C.cid = E.cid
 GROUP BY C.cname
 HAVING count(*) > 1;
+
+/*
+If the name of the column on which two tables are to be joined is the same in both tables, then we can use the USING clause, which takes the joining column name as input.
+*/
+--Find the list of students from "IIT Hyderabad" university who have enrolled in at least one course
+SELECT distinct(S.sid), S.sname, S.age
+FROM StudentDetails S 
+INNER JOIN EnrolledIn E USING(sid)
+WHERE S.university = "IIT Hyderabad";
+
+
+--Find the list of courses where at least one student is enrolled and the price of the course is greater than 4000 and output their course name, price, ratings and Student name.
+SELECT C.cname, C.price, C.ratings, S.name
+FROM CourseDetails C
+INNER JOIN EnrolledIn E ON C.cid = E.cid
+INNER JOIN studentdetails S ON E.sid = S.id
+WHERE C.price > 4000;
