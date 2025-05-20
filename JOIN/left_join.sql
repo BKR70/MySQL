f3acfa5ff7dd647fd04fb0f3e73ece6bea580f4b
Bilas Kumar <153761069+BKR70@users.noreply.github.com>
2025-05-20 14:31:48 +0600

    Update left_join

--- JOIN/left_join.sql
+++ JOIN/left_join.sql
@@ -2,3 +2,11 @@ SELECT employees.name, departments.dept_name
 FROM employees
 LEFT JOIN departments
 ON employees.dept_id = departments.dept_id;
+
+
+/* Look at LEFT_JOIN Database */
+SELECT authors.author_name, COUNT(books.book_id) as book_count
+FROM authors
+LEFT JOIN book_authors ON authors.author_id = book_authors.author_id
+LEFT JOIN books ON book_authors.book_id = books.book_id
+GROUP BY authors.author_name;
