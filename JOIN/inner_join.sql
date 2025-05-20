b35d7c290eeea3fe588f5186cd49922c5b6f3393
Bilas Kumar <153761069+BKR70@users.noreply.github.com>
2025-05-20 12:10:57 +0600

    inner_join

--- JOIN/inner_join.sql
+++ JOIN/inner_join.sql
@@ -2,3 +2,12 @@ SELECT employees.name, departments.dept_name
 FROM employees
 INNER JOIN departments
 ON employees.dept_id = departments.dept_id;
+
+
+/* look at inner_join database. */
+-- Find the list of students of students enrolled in the "Python Fundamentals" course and output their details like student id, name,, and age.
+SELECT S.sid, S.sname, S.age 
+FROM StudentDetails S 
+INNER JOIN EnrolledIn E ON S.sid = E.sid
+INNER JOIN CourseDetails C ON C.cid = E.cid
+WHERE C.cname = "Python Fundamentals";
